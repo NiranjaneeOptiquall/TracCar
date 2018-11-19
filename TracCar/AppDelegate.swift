5b46ff652eed3a2959ab890899e59d456a5807ed
Optiquall Solutions <optiquall@Optiqualls-Mac-mini-2.local>
2018-11-19 12:40:25 +0530

    Initial Commit

---
+++ TracCar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TracCar
+//
+//  Created by Optiquall Solutions on 11/19/18.
+//  Copyright © 2018 Optiquall Solutions. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TracCar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TracCar
+//
+//  Created by Optiquall Solutions on 11/19/18.
+//  Copyright © 2018 Optiquall Solutions. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
